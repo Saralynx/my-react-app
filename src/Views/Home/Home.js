66f61301490c5c429e1ac68d8b9e41814efa413d
saralynx <143858976+Saralynx@users.noreply.github.com>
2023-09-09 20:00:01 +0000

    primer cambiesito

--- src/Views/Home/Home.js
+++ src/Views/Home/Home.js
@@ -8,18 +8,31 @@ export default function Home() {
       className="container-home"
     >
 
-      <a href="/flex"> Go to Flex </a>
+      <a href="/flex"> Go to Flex  2</a>
       <a href="/styled"> Go to Styled </a>
 
       <p>Home</p>
       
       <div className="box" > Box </div>
 
-      <img
-        src={'https://imagizer.imageshack.com/img538/9889/xlMaV6.jpg'}
+      <div className="box2" > Box </div>
+
+      <div style={{backgroundColor: "blue", width: 500, height: 300, margin:20, padding: 80}}>
+        <div style={{backgroundColor: "#9f9f33", padding: 20}}>  <img
+        src={'https://i.pinimg.com/originals/e6/ba/0a/e6ba0a0e1a76e2fad592076f37bc50a3.jpg'}
         className="imageclass"
-      /> 
+      />  </div>
+        <div style={{backgroundColor: "#70163C", padding: 30}}>  <img
+        src={'https://i.pinimg.com/originals/e6/ba/0a/e6ba0a0e1a76e2fad592076f37bc50a3.jpg'}
+        className="imageclass"
+      />  </div>
+      </div>
+
+
+
+
 
     </div>
   )
 }
+
